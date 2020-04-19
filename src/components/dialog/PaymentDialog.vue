a1b6078a0a999c43f49c7a94814927f52f01e546
gn5r <shangyuan.tuolagn@vivaldi.net>
2020-04-20 07:22:00 +0900

    データ作成ダイアログ修正。モックレベルまで作成

--- src/components/dialog/PaymentDialog.vue
+++ src/components/dialog/PaymentDialog.vue
@@ -3,16 +3,12 @@
     <v-card>
       <!-- ダイアログタイトル -->
       <v-card-title class="pa-0">
-        <v-toolbar dark :color="titleColor">
+        <v-toolbar dark color="info">
           <v-toolbar-title class="headline">メモの追加</v-toolbar-title>
           <v-spacer />
           <v-toolbar-items>
             <!-- propsのenableCloseがtrueの時に閉じるボタンを表示(描画)する。デフォルトは非表示 -->
-            <v-btn
-              v-if="enableClose"
-              icon
-              @click="$emit('update:dialog', false)"
-            >
+            <v-btn v-if="enableClose" icon @click="$emit('update:dialog', false)">
               <v-icon>mdi-close</v-icon>
             </v-btn>
           </v-toolbar-items>
@@ -22,25 +18,82 @@
       <v-card-text>
         <v-container fluid>
           <v-row align="center" justify="center">
-            <v-col cols="12">
-              <v-text-field label="タイトル"></v-text-field>
+            <v-col cols="6">
+              <v-text-field label="タイトル" v-model="item.title"></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field label="合計額" v-model="totalCost" readonly></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row align="center" justify="center" v-for="content in item.contents" :key="content.no">
+            <v-col cols="6">
+              <v-text-field label="名称" v-model="content.text"></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field label="金額" v-model="content.cost"></v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row align="center" justify="end" no-gutters>
+            <v-col cols="1">
+              <v-icon color="info" large @click="addData">fas fa-plus-circle</v-icon>
             </v-col>
           </v-row>
         </v-container>
       </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
 export default {
+  name: "PaymentDialog",
   props: {
     dialog: Boolean,
     enableClose: Boolean
   },
-  data: () => ({}),
+  data: () => ({
+    item: {
+      title: "",
+      total: 0,
+      contents: [
+        {
+          no: 1,
+          text: "",
+          cost: 0
+        }
+      ]
+    }
+  }),
 
-  created() {}
+  methods: {
+    addData() {
+      const content = {
+        no: this.item.contents.length + 1,
+        text: "",
+        cost: 0
+      };
+      this.item.contents.push(content);
+    }
+  },
+
+  created() {},
+  computed: {
+    // 合計額を算術プロパティで計算する
+    totalCost: {
+      get() {
+        let total = Number(0);
+        this.item.contents.forEach(content => {
+          total += Number(content.cost);
+        });
+        return total;
+      }
+    }
+  }
 };
 </script>
 
